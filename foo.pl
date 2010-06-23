7e650369420780e575980f093c6eeb292f509a54
Scott Beck <scottbeck@gmail.com>
2010-06-23 17:40:42 -0400

    Build results of 64505dd (on master)

--- foo.pl
+++
@@ -1,9 +0,0 @@
-#!perl
-
-use v5.10.0;
-
-given($foo) {
-    when (/x/) { say '$foo contains an x'; continue }
-    when (/y/) { say '$foo contains a y' }
-    default    { say '$foo does not contain a y' }
-}
